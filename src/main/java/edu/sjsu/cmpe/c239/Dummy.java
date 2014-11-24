905a06108f12608e5098230db0d38a14b0c59429
sumanthv2 <sumanthferrari@gmail.com>
2014-11-24 12:15:09 -0800

    Adding dummy class

---
+++ src/main/java/edu/sjsu/cmpe/c239/Dummy.java
@@ -0,0 +1,9 @@
+package edu.sjsu.cmpe.c239;
+
+public class Dummy {
+	
+	public static void main (String[] args) {
+		System.out.println("This is a dummy class");
+	}
+
+}
---
+++ src/test/java/edu/sjsu/cmpe/c239/DummyTest.java
@@ -0,0 +1,11 @@
+package edu.sjsu.cmpe.c239;
+
+public class DummyTest {
+
+	public static void main (String[] args) {
+		
+		System.out.println("This is a DummyTest");
+		
+	}
+		
+}
